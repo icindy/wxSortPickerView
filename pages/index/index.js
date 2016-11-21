af3b4826bdabd93d4e87aa7e09490b98688e67cd
ismoker <uestczhangdi@163.com>
2016-11-21 21:04:19 +0800

    1.加入动作

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,6 +4,7 @@ var app = getApp()
 var wxSortPickerView = require('../../wxSortPickerView/wxSortPickerView.js');
 Page({
   data: {
+    nimationData: {},
     wxSortPickerData:{textData:[{tag:"A",textArray:["啊","嗄","啊","嗄","啊","嗄","啊","嗄","啊","嗄","啊","嗄","啊","嗄","啊","嗄","啊","嗄","啊","嗄"]},{tag:"B",textArray:[]},{tag:"A",textArray:["啊","嗄"]}]}
   },
   //事件处理函数
@@ -19,5 +20,28 @@ Page({
     that.setData({
       testData:testData
     })
+  },
+  onShow: function(){
+    var animation = wx.createAnimation({
+      duration: 1000,
+        timingFunction: 'ease',
+    })
+
+    this.animation = animation
+
+    animation.scale(2,2).rotate(45).step()
+    var temData = this.data.wxSortPickerData;
+    temData.animationData = animation.export()
+    this.setData({
+      wxSortPickerData:temData,
+      animationData:animation.export()
+    })
+
+    setTimeout(function() {
+      animation.translate(30).step()
+      this.setData({
+        animationData:animation.export()
+      })
+    }.bind(this), 1000)
   }
 })
